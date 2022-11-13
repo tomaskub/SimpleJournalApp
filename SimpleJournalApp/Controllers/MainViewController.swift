84052633435dedbb530d2d539a0aa57d76f83d01
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2022-11-13 17:46:50 +0100

    Implement calendarButton: -configure the view programmaticly -add IBDesignable functionality -add method setTopLabelText and setBottomLabelText
    Delete xib file for calendarButton
    
    Remove UIButton array in MainViewController
    Add CalendarButtonArray in MainViewController
    Remove ScrollViewContentView and replace anchors to reference ScrollViewDirectly

--- SimpleJournalApp/Controllers/MainViewController.swift
+++ SimpleJournalApp/Controllers/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+    //placeholder questions
     let questions: [String] = [
         "Summary of the day",
         "What did i do good?",
@@ -16,6 +16,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         "How can I improve on that?",
         "Where was my discipline and self-control tested?"]
     
+    //tableView method implementation
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return questions.count
     }
@@ -28,32 +29,28 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return cell
     }
     
-
-    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var dateLabel: UILabel!
-    
-    @IBOutlet weak var leftLabel: UILabel!
-    @IBOutlet weak var scrollViewContentView: UIView!
-    @IBOutlet weak var journalStartButton: UIButton!
-    var dateButtonArray: [UIButton] = {
-        var tempArray: [UIButton] = []
-        for i in 0...13 {
-            let button: UIButton = {
-                let button = UIButton()
-                let date = Calendar.current.date(byAdding: .day, value: -7+i, to: Date.now)
-                button.setTitle(String(date!.formatted(date: .complete, time: .omitted).prefix(3)), for: .normal)
-                button.setTitleColor(.white, for: .normal)
-                button.translatesAutoresizingMaskIntoConstraints = false
-                if i == 7 {
-                    button.backgroundColor = .black
-                } else {
-                    button.backgroundColor = .darkGray
-                }
-                return button
+    @IBOutlet weak var scrollView: UIScrollView!
+    //Calendar buttons
+    var dateButtonArray: [CalendarDayButton] = {
+        var tempArray: [CalendarDayButton] = []
+            for i in 0...13 {
+                let button: CalendarDayButton = {
+                    let button = CalendarDayButton()
+                    let date = Calendar.current.date(byAdding: .day, value: -7+i, to: Date.now)
+                    // set the label text on the buttons
+                    button.setTopLabelText(text: //String(date!.formatted(date: .abbreviated, time: .omitted).prefix(2)))
+                                           String(Calendar.current.dateComponents([.day], from: date!).day!))
+                    button.setBottomLabelText(text: String(date!.formatted(date: .complete, time: .omitted).prefix(3).uppercased()))
+                    // change color on current day button
+                    if i == 7 {
+                        button.primaryColor = .black
+                    }
+                    button.translatesAutoresizingMaskIntoConstraints = false
+                    return button
             }()
-            tempArray.append(button)
-            
+                tempArray.append(button)
         }
         return tempArray
     }()
@@ -63,36 +60,34 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        journalStartButton.titleLabel?.text = "Answer tha questions!"
+
         dateLabel.text = Date.now.formatted(date: .complete, time: .omitted)
-        for i in 0...dateButtonArray.count-1 {
-            scrollViewContentView.addSubview(dateButtonArray[i])
-            dateButtonArray[i].topAnchor.constraint(equalTo: scrollViewContentView.topAnchor).isActive = true
-            dateButtonArray[i].bottomAnchor.constraint(equalTo: scrollViewContentView.bottomAnchor).isActive = true
-            switch i {
-            case 0:
-                dateButtonArray[i].leadingAnchor.constraint(equalTo: scrollViewContentView.leadingAnchor, constant: 10).isActive = true
-            default:
-                dateButtonArray[i].leadingAnchor.constraint(equalTo: dateButtonArray[i-1].trailingAnchor, constant: 10).isActive = true
+        
+        // set up constraints for calendar buttons
+            for i in 0...dateButtonArray.count-1 {
+                scrollView.addSubview(dateButtonArray[i])
+                
+                dateButtonArray[i].topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+                dateButtonArray[i].bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+                
+                switch i {
+                case 0:
+                    dateButtonArray[i].leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10).isActive = true
+                default:
+                    dateButtonArray[i].leadingAnchor.constraint(equalTo: dateButtonArray[i-1].trailingAnchor, constant: 10).isActive = true
+                }
+                
+                dateButtonArray[i].widthAnchor.constraint(equalToConstant: 85).isActive = true
+                dateButtonArray[i].heightAnchor.constraint(equalToConstant: 100).isActive = true
+                
             }
-            
-            dateButtonArray[i].widthAnchor.constraint(equalToConstant: 50).isActive = true
-            
-        }
+        dateButtonArray.last?.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 10).isActive = true
+        // set up table view
         tableView.rowHeight = 80
         tableView.delegate = self
         tableView.dataSource = self
-        
         tableView.reloadData()
-//        scrollViewContentView.trailingAnchor.constraint(equalTo: dateButtonArray.last!.trailingAnchor).isActive = true
-        dateButtonArray.last!.trailingAnchor.constraint(equalTo: scrollViewContentView.trailingAnchor).isActive = true
-        // Do any additional setup after loading the view.
     }
-    @IBAction func journalStartButtonTapped(_ sender: Any) {
-
-        dateLabel.text = Date.now.formatted(date: .complete, time: .omitted)
-    }
-    
 
 }
 
--- SimpleJournalApp/Views/CalendarDayButton.swift
+++ SimpleJournalApp/Views/CalendarDayButton.swift
@@ -9,23 +9,104 @@ import UIKit
 
 @IBDesignable class CalendarDayButton: UIButton {
 
-    @IBOutlet private weak var dayNameLabel: UILabel!
-    @IBOutlet private weak var dayNumberLabel: UILabel!
+    @IBInspectable var secondaryColor: UIColor = UIColor(red: 251.0/255.0, green: 251.0/255.0, blue: 251.0/255.0, alpha: 1)
+    @IBInspectable var primaryColor: UIColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 1)
+    @IBInspectable var tertiaryColor: UIColor = UIColor(red: 139/255, green: 139/255, blue: 139/255, alpha: 1)
+    @IBInspectable var isTodayButton: Bool = false
+    @IBInspectable var bottomLabelText: String = "DAY"
+    @IBInspectable var topLabelText: String = "NN"
     
-    public func setDate(date: Date){
-        dayNameLabel.text = String(date.formatted(date: .complete, time: .omitted).prefix(3))
-        dayNumberLabel.text = String(date.formatted(date: .numeric, time: .omitted).prefix(2))
+    let bottomLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let topLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private struct Constants {
+        static let plusLineWidth: CGFloat = 3.0
+        static let plusButtonScale: CGFloat = 0.6
+        static let halfPointShift: CGFloat = 0.5
+    }
+    private var halfWidth: CGFloat {
+        return bounds.width / 2
+    }
+    private var halfHeight: CGFloat {
+        return bounds.height / 2
+    }
+    private var mainDimension: CGFloat {
+        if 1+bounds.height == bounds.width {
+            return 0.85 * bounds.height
+        } else {
+            
+            return min (bounds.height, bounds.width)
+            
+        }
     }
     
-    
-//    override sele
-    
-    /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        
+        // main card rectangle
+        let path = UIBezierPath(roundedRect: CGRect(x: 0, //mainDimension * 0.15 / 2,
+                                                    y: 0, width: mainDimension, height: mainDimension*1.2), cornerRadius: mainDimension * 0.15)
+        secondaryColor.setFill()
+        path.fill()
+//        path.lineWidth = Constants.plusLineWidth / 2
+//        UIColor.black.setStroke()
+//        path.stroke()
+        // calendar card rectangle
+        let secondaryPath = UIBezierPath(roundedRect: CGRect(x: mainDimension * 0.1, y: mainDimension * 0.1, width: mainDimension * 0.8 , height: mainDimension * 0.7), cornerRadius: 0.15 * mainDimension)
+        primaryColor.setFill()
+        secondaryPath.fill()
+        
+        if isTodayButton {
+            let plusWidth = min(bounds.width, bounds.height) * Constants.plusButtonScale
+            let halfPlusWidth = plusWidth / 2
+            let plusPath = UIBezierPath()
+            plusPath.lineWidth = Constants.plusLineWidth
+            plusPath.move(to: CGPoint(x: halfWidth - halfPlusWidth, y: halfHeight))
+            plusPath.addLine(to: CGPoint(x: halfWidth + halfPlusWidth, y: halfHeight))
+            plusPath.move(to: CGPoint(x: halfWidth, y: halfHeight + halfPlusWidth))
+            plusPath.addLine(to: CGPoint(x: halfWidth, y: halfHeight-halfPlusWidth))
+            UIColor.white.setFill()
+            plusPath.stroke()
+        }
+        
+        //Set up button labels
+        bottomLabel.text = bottomLabelText
+        bottomLabel.textColor = tertiaryColor
+        
+        topLabel.text = topLabelText
+        topLabel.font = .systemFont(ofSize: mainDimension * 0.3 , weight: .bold)
+        topLabel.textColor = secondaryColor
+        
+        self.addSubview(bottomLabel)
+        bottomLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -0.10 * mainDimension).isActive = true
+        bottomLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        self.addSubview(topLabel)
+        topLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -0.57 * mainDimension).isActive = true
+        topLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        
     }
-    */
+    
+    func setTopLabelText(text: String) -> Void {
+//        topLabel.text = text
+        topLabelText = text
+//        self.setNeedsUpdateConfiguration()
+    }
+    func setBottomLabelText(text: String) -> Void {
+        bottomLabelText = text
+    }
     
 }
