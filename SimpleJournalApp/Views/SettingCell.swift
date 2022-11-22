6589ebe4fc7cf285a4ba8b96436d07c298147d1e
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2022-11-22 19:18:33 +0100

    Add styling and visuals for SettingCell

--- SimpleJournalApp/Views/SettingCell.swift
+++ SimpleJournalApp/Views/SettingCell.swift
@@ -34,7 +34,7 @@ import UIKit
     }()
     private let button: UIButton = {
        let button = UIButton()
-        button.setTitle("button", for: .normal)
+//        button.setTitle("button", for: .normal)
         button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -44,33 +44,48 @@ import UIKit
         toggle.translatesAutoresizingMaskIntoConstraints = false
         return toggle
     }()
+    private let cellContentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        self.cellType = .withToggleSwitch
+        self.cellType = .withChevronRight
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         // Define background view
-        self.backgroundColor = .white
-        self.layer.cornerRadius = 5
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowRadius = 5
-        
-        
         //Set up appearance of the cell
-        contentView.layer.cornerRadius = contentView.bounds.height * 0.5
-        contentView.layer.shadowColor = UIColor.black.cgColor
-        contentView.layer.shadowRadius = 5.0
-        label.backgroundColor = .blue
-        contentView.addSubview(label)
+        //TODO: figure out how to properly style the cell base on fitwifeapp
+        //inset frame by 10
+//        cellContentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10  ))
+        //set up corner radius
+        cellContentView.layer.cornerRadius = contentView.frame.height * 0.5
+//        cellContentView.layer.borderWidth = 3.0
+//        cellContentView.layer.borderColor = UIColor.black.cgColor
+        //set up shadow
+        cellContentView.backgroundColor = UIColor(named: "ComplementColor")
+        cellContentView.layer.shadowOffset = .zero
+        cellContentView.layer.shadowColor = UIColor.black.cgColor
+        cellContentView.layer.shadowRadius = 2.0
+        cellContentView.layer.shadowOpacity = 0.5
+        // set up constraints for cellContentView
+        
+        //TODO: add logic to change icon in the view
+        //Setup icon image and frame (is frame needed?)
         icon.image = UIImage(systemName: iconSystemName)
         icon.frame = CGRect(x: 0, y: 0, width: 75, height: 50)
         icon.contentMode = .scaleAspectFit
-        contentView.addSubview(icon)
+        
+        // add subviews
+        contentView.addSubview(cellContentView)
+        cellContentView.addSubview(label)
+        cellContentView.addSubview(icon)
         //Build cell based on the cell type
         switch cellType {
         case .withChevronRight:
-            addSubview(button)
+            cellContentView.addSubview(button)
             setUpConstraints(first: icon, second: label, third: button)
         case .withToggleSwitch:
-            addSubview(toggleSwitch)
+            cellContentView.addSubview(toggleSwitch)
             setUpConstraints(first: icon, second: label, third: toggleSwitch)
         }
     }
@@ -99,20 +114,24 @@ import UIKit
     }
 
     func setUpConstraints(first: UIView, second: UIView, third: UIView){
+        cellContentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+        cellContentView.topAnchor.constraint(equalTo: contentView.topAnchor, constant:  5).isActive = true
+        cellContentView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5).isActive = true
+        cellContentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
         //set up constraints for first item
-        first.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
-        first.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10.0).isActive = true
+        first.centerYAnchor.constraint(equalTo: cellContentView.centerYAnchor).isActive = true
+        first.leadingAnchor.constraint(equalTo: cellContentView.leadingAnchor, constant: 10.0).isActive = true
         first.widthAnchor.constraint(equalToConstant: contentView.frame.height - 10).isActive = true
         first.heightAnchor.constraint(equalToConstant: contentView.frame.height - 10).isActive = true
         // set up constraints for second item
-        second.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        second.centerYAnchor.constraint(equalTo: cellContentView.centerYAnchor).isActive = true
         second.leadingAnchor.constraint(equalTo: first.trailingAnchor, constant: 10).isActive = true
         second.widthAnchor.constraint(equalToConstant: 150).isActive = true
         second.heightAnchor.constraint(equalToConstant: contentView.frame.height - 10 ).isActive = true
         // set up constraints for third item
-        third.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
-        third.leadingAnchor.constraint(equalTo: second.trailingAnchor).isActive = true
-        third.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        third.centerYAnchor.constraint(equalTo: cellContentView.centerYAnchor).isActive = true
+//        third.leadingAnchor.constraint(equalTo: second.trailingAnchor).isActive = true
+        third.trailingAnchor.constraint(equalTo: cellContentView.trailingAnchor, constant: -10).isActive = true
         third.heightAnchor.constraint(equalToConstant: contentView.frame.height).isActive = true
         
     }
