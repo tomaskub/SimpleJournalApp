317ddc0bc9251839b239b140fc76ff5f7b49d685
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2022-12-30 01:28:44 +0100

    Implement UI based on QuestionCard and scrollView

--- SimpleJournalApp/Controllers/HistoryDetailViewController.swift
+++ SimpleJournalApp/Controllers/HistoryDetailViewController.swift
@@ -9,29 +9,62 @@ import UIKit
 
 class HistoryDetailViewController: UIViewController {
     
+    //MARK: UI elements
     
+    private let scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isPagingEnabled = true
+        return view
+    }()
+    private let questionCards: [QuestionView] = {
+        var cards: [QuestionView] = []
+        for question in K.questions {
+            let card = QuestionView()
+            card.translatesAutoresizingMaskIntoConstraints = false
+            card.configure(question: question)
+            cards.append(card)
+        }
+        return cards
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let label = UILabel()
-        label.text = "This is HistoryDetailsViewController"
-        label.translatesAutoresizingMaskIntoConstraints = false 
-        view.addSubview(label)
-        NSLayoutConstraint.activate([
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(named: K.Colors.dominant)
+        addViews()
+        layoutUI()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: UI layout
+    private func addViews() {
+        for card in questionCards {
+            scrollView.addSubview(card)
+        }
+        view.addSubview(scrollView)
     }
-    */
-
+    private func layoutUI() {
+        for card in questionCards {
+            NSLayoutConstraint.activate([
+                card.heightAnchor.constraint(equalToConstant: view.frame.height),
+                card.widthAnchor.constraint(equalToConstant: view.frame.width)
+            ])
+        }
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        questionCards.first?.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        for i in 1...questionCards.count - 1 {
+            NSLayoutConstraint.activate([
+                questionCards[i].leadingAnchor.constraint(equalTo: questionCards[i-1].trailingAnchor)])
+        }
+        questionCards.last?.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+    }
+    
 }
+
+
