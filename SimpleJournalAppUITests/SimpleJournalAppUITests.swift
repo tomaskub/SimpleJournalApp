4a4c0a4e6c8ea20f0212a3c0c3dc691387acd4d0
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-01-31 20:47:06 +0100

    Add testAddingNewEntry to UI tests

--- SimpleJournalAppUITests/SimpleJournalAppUITests.swift
+++ SimpleJournalAppUITests/SimpleJournalAppUITests.swift
@@ -9,24 +9,51 @@ import XCTest
 
 class SimpleJournalAppUITests: XCTestCase {
 
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        try super.setUpWithError()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        app = XCUIApplication()
+        app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
+    func testAddingNewEntry() throws {
+        
+        app.tables.cells.containing(.staticText, identifier:"Journal")/*@START_MENU_TOKEN@*/.staticTexts["Answer"]/*[[".buttons[\"Answer\"].staticTexts[\"Answer\"]",".staticTexts[\"Answer\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        
+        let scrollViewsQuery = app.scrollViews
+        
+        let summaryOfTheDayElement = scrollViewsQuery.otherElements.containing(.staticText, identifier:"Summary of the day").element
+        let summaryTextView = summaryOfTheDayElement.children(matching: .textView).firstMatch
+        summaryTextView.tap()
+        summaryTextView.typeText("This is a summary of the day")
+        summaryOfTheDayElement.swipeLeft()
+        
+        let textView = scrollViewsQuery.otherElements.containing(.staticText, identifier:"What did i do good?").children(matching: .textView).element
+        textView.tap()
+        textView.typeText("This is what i did good")
+//        textView.swipeLeft()
+        
+        
+        
+        
+        
+        textView.swipeDown(velocity: .fast)
+        
+        
+        let tabBar = XCUIApplication().tabBars["Tab Bar"]
+        
+        tabBar.buttons["chart column"].tap()
+        
+        XCTAssertTrue(app.tables.cells.staticTexts["31.01.2023"].exists)
+        
+        
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
